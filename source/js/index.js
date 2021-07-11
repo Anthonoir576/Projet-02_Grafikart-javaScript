1c77186936f1513ca5a261dee71209317ff68c35
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-11 15:17:39 +0200

    version 0.2 - structure js - ok

--- source/js/index.js
+++ source/js/index.js
@@ -15,10 +15,19 @@
 
             // Supprimer la class active
             let navigation = this.parentNode.parentNode.parentNode
+
             navigation.querySelector('.tabs--nav .active').classList.remove('active');
 
             // créer la nouvelle class active
             this.parentNode.classList.add('active');
+
+            // on enleve la class active a la div pour la faire disparaitre
+            let body = document.body
+
+            body.querySelector('.div--content.active').classList.remove('active');
+
+            // on ajoute la class active a la div ciblé
+            body.querySelector(this.getAttribute('href')).classList.add('active'); 
 
         });
 
