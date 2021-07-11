3e7af278f13b4009a80c4ca3f39257f95d7ad9fe
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-11 14:56:26 +0200

    version 0.2 - structure js - en cours

--- source/js/index.js
+++ source/js/index.js
@@ -1,7 +1,31 @@
 (function() {
 
+    let listes = document.querySelectorAll('.tabs--nav a');
 
+    for (let i = 0; i < listes.length; i++) {
 
+        listes[i].addEventListener('click', function(e) {
 
+            // verifie si le liens et deja active
+            if (this.parentNode.classList.contains('active')) {
+
+                return false
+
+            }
+
+            // Supprimer la class active
+            let navigation = this.parentNode.parentNode.parentNode
+            navigation.querySelector('.tabs--nav .active').classList.remove('active');
+
+            // créer la nouvelle class active
+            this.parentNode.classList.add('active');
+
+        });
+
+    };
+
+    let contents = document.querySelectorAll('.tabs--content div');
+
+ 
 
 })();
